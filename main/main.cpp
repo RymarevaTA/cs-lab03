7ed0bd177a80f5c5ec0ab9f45f7d9103979fe42e
Tanya (rymareva) <tanya@example.ru>
2020-04-17 14:21:55 +0300

    code: графический вывод гистограммы

--- main/main.cpp
+++ main/main.cpp
@@ -90,9 +90,53 @@ void show_histogram_text(vector<size_t> bins) {
             cout << '*';
         }
         cout << '\n';
-    }
-
+    } }
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
 }
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, size_t text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text <<"</text>";
+}
+void svg_rect(double x, double y, double width, double height, string stroke="black", string fill="black") {
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
+}
+ /*void show_histogram_svg(const vector<size_t>& bins) {
+    svg_begin(400, 300);
+    svg_text(20, 20, to_string(bins[0]));
+    svg_rect(50, 0, bins[0] * 10, 30);
+    svg_end();
+} */
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, bin);
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
 
 
 int main() {
@@ -113,7 +157,7 @@ int main() {
     const auto bins = make_histogram(numbers, bin_count);
 
     // Вывод данных
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 
     return 0;
 }
