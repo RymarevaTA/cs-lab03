a9385267d3a2011db7c3819d71615539f5c836d5
Tanya (RymarevaTA) <rymareva@example.ru>
2020-04-29 11:54:08 +0300

    code: масштабирование гистограммы

--- main/svg.cpp
+++ main/svg.cpp
@@ -17,7 +17,7 @@ void svg_end() {
     cout << "</svg>\n";
 }
 
-void svg_text(double left, double baseline, size_t text) {
+void svg_text(double left, double baseline, string text) {
     cout << "<text x='" << left << "' y='" << baseline << "'>" << text <<"</text>";
 }
 
@@ -26,21 +26,40 @@ void svg_rect(double x, double y, double width, double height, string stroke, st
 }
 
 void show_histogram_svg(const vector<size_t>& bins) {
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
+const size_t MAX_ASTERISK=35;
+const auto IMAGE_WIDTH = 400;
+const auto IMAGE_HEIGHT = 300;
+const auto TEXT_LEFT = 20;
+const auto TEXT_BASELINE = 20;
+const auto TEXT_WIDTH = 50;
+const auto BIN_HEIGHT = 30;
+const auto BLOCK_WIDTH = 10;
+double top = 0;
+svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+for (size_t bin : bins) {
+const double bin_width = BLOCK_WIDTH * bin;
+double top = 0;
+unsigned max_count=0;
+for (size_t bin : bins)
+{
+if(max_count<bin)
+max_count=bin;
+}
+for (size_t bin : bins)
+    {
+    size_t height=35;
+if(max_count>MAX_ASTERISK)
+height=MAX_ASTERISK*((static_cast<double>(bin))/max_count);
+else
+{
+height=bin;
+}
+height = BLOCK_WIDTH * height;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, height, BIN_HEIGHT,"black","green");
 
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-    for (size_t bin : bins) {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, bin);
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
-        top += BIN_HEIGHT;
-    }
+    top += BIN_HEIGHT;
+}
     svg_end();
+}
 }
