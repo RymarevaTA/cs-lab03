bfdf5515d20a52fd5df4e8a3bf54c880189d6235
Tanya (RymarevaTA) <tanya@example.ru>
2020-05-26 17:57:23 +0300

    code: сохранение данных из сети в буфер

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
-#include <curl/curl.h> //сторонн€€ библиотека
+#include <curl/curl.h>
+#include <sstream>
+#include <string>
 using namespace std;
 
 
@@ -66,29 +68,48 @@ vector<size_t> make_histogram(const Input& data)
     return result;
 }
 
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    const size_t data_size= item_size*item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(new_items, data_size);
+    return data_size;
+}
+
+Input
+download(const string& address)
+{
+    stringstream buffer;
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL* curl = curl_easy_init();
+    if(curl)
+    {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        if (res!= CURLE_OK)
+        {
+            cerr<<curl_easy_strerror(res);
+            exit(1);
+        }
+        curl_easy_cleanup(curl);
+    }
+
+    return read_input(buffer, false);
+}
+
+
 int main(int argc, char* argv[])
 {
+    Input input;
     if (argc>1)
-    {
-        CURL* curl = curl_easy_init();
-        if(curl)
-        {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-            if (res!= CURLE_OK){
-                cerr<<curl_easy_strerror(res);
-                exit(1);
-            }
-            curl_easy_cleanup(curl);
-        }
-        return 0;
-    }
-    curl_global_init(CURL_GLOBAL_ALL);
-
-
-    const auto input = read_input(cin, true);
+    input=download(argv[1]);
+    else
+    input = read_input(cin, true);
     const auto bins=make_histogram(input);
     show_histogram_svg(bins);
     return 0;
